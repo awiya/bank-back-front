1aea0a86e21d29e338c2b920cd29c37dc1566a40
AYOUB <a.idihia@gmail.com>
2023-04-17 11:52:44 +0200

    Creating DTOs

---
+++ src/main/java/io/awiya/bankback/dtos/BankAccountDTO.java
@@ -0,0 +1,8 @@
+package io.awiya.bankback.dtos;
+
+import lombok.Data;
+
+@Data
+public class BankAccountDTO {
+    private String type;
+}
---
+++ src/main/java/io/awiya/bankback/dtos/CustomerDTO.java
@@ -0,0 +1,10 @@
+package io.awiya.bankback.dtos;
+
+import lombok.Data;
+
+@Data
+public class CustomerDTO {
+    private Long id;
+    private String name;
+    private String email;
+}
